e15a8b3475bdf6a94595f7565f937fa5551c6e28
HarryG <harrygg@gmail.com>
2016-02-28 08:37:56 +0200

    update

--- plugin.video.bgcameras/addon.py
+++ plugin.video.bgcameras/addon.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 import sys
-from xbmcswift2 import Plugin
-#from resources.lib.assets import *
+from xbmcswift2 import Plugin, xbmc, xbmcgui, xbmcplugin, xbmcaddon
+from resources.lib.assets import *
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 plugin = Plugin()
+addon_id = 'plugin.video.bgcameras'
+addon = xbmcaddon.Addon(id = addon_id)
+profile = xbmc.translatePath(addon.getAddonInfo('profile'))
+afp = os.path.join(profile, 'assets.sqlite')
 
-categories = ['София', 'Пловдив', 'Други']
 category = 0
-
-#items = [{
-#  'label': cat,
-#  'path': plugin.url_for('show_category'),
-#  'is_playable': False
-#} for cat in categories]
-
 
 #plugin entry screen Categories
 @plugin.route('/')
 def index():
-  items = [{'label': categories[i], 'path': plugin.url_for('show_category', category=i), 'is_playable': False} for i in range (0, len(categories))]
-  return items
+	cats = GetCategories(afp)
+	items = [{
+		'label': cat.name, 
+		'path': plugin.url_for('show_category', category=cat.id), 
+		'is_playable': False
+	}	for cat in cats]
+	return items
 
 #Display videos for the provided category
 @plugin.route('/<category>/')
@@ -30,10 +31,6 @@ def show_category(category):
   #items = get_items(category) 
   #return plugin.finish(items)
   pass
-
-#categories = ['София', 'Пловдив', 'Други']
-
-
 
 
 if __name__ == '__main__':
